81568d7069015e866dbc22a6491ea8dde637f87e
amyclaussen <aeclaussen@gmail.com>
2016-08-13 15:45:36 -0700

    refactoring the shoplist homework to go from lists to a dictionary

--- countletters.py
+++ countletters.py
@@ -3,10 +3,7 @@ lettercount_dict = {}
 def count_letters():
 	myname = raw_input("What is your name?").lower()
 	for i in myname:
-
-		#lettershowsup = count how many times i appears
-
-		lettercount_dict[i] = "dunnoyet" #change this to lettershowsup
+		lettercount_dict[i] = myname.count(i)
 	print lettercount_dict
 	
 
--- shoplist.py
+++ shoplist.py
@@ -1,44 +1,75 @@
-shoplist = []
+shoplist = {"main":["eggs"]}
+
+def show_specific_list(shoplist):
+    print "Current lists are:", shoplist.keys()
+    print shoplist[raw_input("Which list would you like to view? ").lower()]
+
+def add_list(shoplist):
+    name_list = raw_input("Which list would you like to add? ").lower()
+    shoplist[name_list] = []
+    print "New list added:", name_list
 
 def add_item(shoplist):
-    item = raw_input("Which item would you like to add? ").lower()
-    if item in shoplist:
+    print "Current lists are:", shoplist.keys()
+    name_list = raw_input("Which list would you like to add to? ").lower()
+    name_item = raw_input("Which item would you like to add? ").lower()
+    if name_item in shoplist[name_list]:
         print "This item is already in the list."
     else:
-        shoplist.append(item)
-        print "Added to list:", item
+        shoplist[name_list] = name_item
+        print "Added to " + name_list + ": " + name_item
 
 def remove_item(shoplist):
-    item = raw_input("Which item would you like to remove? ").lower()
-    if item not in shoplist:
+    print "Current lists are:", shoplist.keys()
+    name_list = raw_input("Which list would you like to remove from? ").lower()
+    print "Current items are:", shoplist[name_list]
+    name_item = raw_input("Which item would you like to remove? ").lower()
+    name_item_for_print = name_item
+    if name_item not in shoplist[name_list]:
         print "This item is not in the list."
-    else:
-        shoplist.remove(item)
-        print "Removed from list:", item
+    # else:
+    #     for i in shoplist[name_list]:
+    #         if i == name_item:
+    #             name_list.remove(name_item)
+    #             #'str' object has no attribute 'remove'
+    #     print "Removed from " + name_list + ": " + name_item_for_print
+
+def remove_list(shoplist):
+    name_list = raw_input("Which list would you like to remove? ").lower()
+    del shoplist[name_list]
 
 def menu():
     while True:
         print ""
         print "Select a Menu Option:"
-        print "     1 - Show Current List"
-        print "     2 - Add Item"
-        print "     3 - Remove Item"
-        print "     4 - Quit"
+        print "     1 - Show All Lists"
+        print "     2 - Show Specific List"
+        print "     3 - Add New List"
+        print "     4 - Add Item"
+        print "     5 - Remove Item"
+        print "     6 - Remove List"
+        print "     7 - Quit"
         choice = raw_input("-->  ")
-        if choice not in ["1","2","3","4"]:
+        if choice not in str(range(8)):
             print "'" + str(choice) + "' was not a menu item."
         else:
             return int(choice)
 
 def main():
     choice = None
-    while choice != 4:
+    while choice != 7:
         choice = menu()
         if choice == 1:
             print shoplist
         elif choice == 2:
-            add_item(shoplist)
+            show_specific_list(shoplist)
         elif choice == 3:
+            add_list(shoplist)
+        elif choice == 4:
+            add_item(shoplist)
+        elif choice == 5:
+            remove_item(shoplist)
+        elif choice == 6:
             remove_item(shoplist)
         else:
             print "Goodbye."
